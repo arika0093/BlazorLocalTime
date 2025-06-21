03044cf23aa528ffead0a157f96bb8fa18865851
Arika Ishinami <delete0093@gmail.com>
2025-06-21 15:56:30 +0900

    Add support for MudBlazor, Fluent UI, and Ant Design in LocalTime components and update styles

--- example/BlazorLocalTimeSample/Program.cs
+++ example/BlazorLocalTimeSample/Program.cs
@@ -1,6 +1,8 @@
 using BlazorLocalTimeSample;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,6 +10,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Add localtime service
 builder.Services.AddBlazorLocalTimeService();
+
+// for sample
+builder.Services.AddFluentUIComponents();
+builder.Services.AddMudServices();
+builder.Services.AddAntDesign();
 
 builder.Services.AddScoped(sp => new HttpClient
 {
